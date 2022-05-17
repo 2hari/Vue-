b05820e0d34302d3ba93d74f30be84eb69bc6223
Hari <two1821125run@protonmail.com>
2022-05-17 15:07:27 +0530

    vue forms
    two way data binding with v-model, keyboard even and modifiers, preventing default broser action for form submission, vaiation

---
+++ forms/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <SignUpForm/>
+</template>
+
+<script>
+import SignUpForm from './components/SignUpForm.vue'
+
+export default{
+  name:'vue forms',
+  components:{SignUpForm}
+}
+</script>
+
+<style>
+
+</style>
---
+++ forms/src/components/SignUpForm.vue
@@ -0,0 +1,60 @@
+<template>
+  <form>
+    <label>Email:</label>
+    <input type="email" required v-model="email">
+    <label>Password:</label>
+    <input type="password" required v-model="password">
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
+    <label >accept terms and conditions</label>
+    <input type="checkbox" v-model="terms" required>
+    <label >shaun</label>
+    <input value="shaun" type="checkbox" v-model="names"  >
+    <label >yoshi</label>
+    <input value="yoshi" type="checkbox" v-model="names"  >
+    <label >mario</label>
+    <input value="mario" type="checkbox" v-model="names"  >
+    <label >skill</label>
+    <input type="text" v-model="tempSkill" @keyup.alt="addSkill">
+  </form>
+  <p>{{password}},{{email}}</p>
+  <p>{{role}},{{terms}}</p>
+  <p>{{names}}</p>
+  <div v-for="skill in skills" :key="skill">
+  {{skill}}
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      email:'',
+      password:'',
+      role:'',
+      terms: false,
+      names:[],
+      tempSkill:'',  
+      skills:[]
+    }
+  },
+  methods:{
+    addSkill(e){
+      if(e.key === ',' && this.tempSkill){
+        if(!this.skills.includes(this.tempSkill)){
+          this.skills.push(this.tempSkill)
+        }
+          this.tempSkill =''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  
+</style>
+
