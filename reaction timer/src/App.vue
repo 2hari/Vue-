93445e6058d48886022ff22cef53b0e7936e21ea
Hari <two1821125run@protonmail.com>
2022-05-17 13:44:22 +0530

    reaction timer
    @event, :var, v-if, v-show, v-for, dynamic classes, computed properties, default arg e in event handler, vue cli SPA, multiple SFCs, props, custom event emitters, even modifers, v-slot for advanced childrens, life-cycle methods.

---
+++ reaction timer/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="showResults" :score="score"/>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
+export default {
+  name:'Reaction time app',
+  components:{Block,Results},
+  data(){
+    return {
+      isPlaying:false,
+      delay:null,
+      score:null,
+      showResults:false
+    }
+  },
+  methods:{
+    start(){
+      this.delay = 500 + Math.random()*2000
+      this.isPlaying = true
+      this.showResults = false
+     },
+     endGame(reactionTime){
+       this.score = reactionTime
+       this.isPlaying = false
+       this.showResults = true
+     }
+  }
+}
+</script>
+
+
+<style>
+
+</style>
